0a6a6505ede5fba43b245547dc40840824026e0c
André de Alencar <andre.alencar.p@gmail.com>
2017-02-02 14:09:36 -0200

    regex

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ bot.onText(/\/soletre (.+)/, function (msg, match) {
   bot.sendMessage(chatId, spelled);
 });
 
-bot.onText(/\/selic (.+)/, function (msg, match) {
+bot.onText(/\/selic (.*)/, function (msg, match) {
   var chatId = msg.chat.id;
   var resp = match[1] || 1;
   console.log('resp: ' + resp);
@@ -108,33 +108,6 @@ bot.onText(/\/selic (.+)/, function (msg, match) {
   
 });
 
-bot.onText(/\/selic/, function (msg) {
-  var chatId = msg.chat.id;
-  var resp = 1;
-  console.log('resp: ' + resp);
-  var result = "";
-  
-  request({
-    url: 'http://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados/ultimos/' + resp +'?formato=json',
-    json: true
-  }, function (error, response, body) {
-
-    if (!error && response.statusCode === 200) {
-			console.log("if ok");
-			for (var i = 0; i < body.length; i++) {
-				result += "data: " + body[i].data + "\nvalor: " + body[i].valor + "\n\n";
-			}
-			 bot.sendMessage(chatId, result);
-    } else {
-			console.log("else ok");
-			result = "Falha";
-			bot.sendMessage(chatId, result);
-	}
-  });
-  
- 
-  
-});
 
 /*
 bot.on('inline_query', function (msg) {
